fde3b30daceb4c434d953bfaac49ed4260fe0a79
mrvux <julien@mrvux.com>
2015-04-25 15:15:09 +0100

    [Cleanup] Remove all bin/obj folders from

--- Z3Experiments/Z3Experiments/bin/x64/Debug/runBenchmark.ps1
+++
@@ -1,8 +0,0 @@
-
-$files = get-childitem . *.smt2|where-object {!($_.psiscontainer)}
-$z3path = "c:\users\dmolnar\Documents\z3bin\bin\z3.exe -st -T:3000 -t:30000"
-foreach ($file in $files)
-{
-    $cmd = "$z3path $file"
-    Invoke-Expression $cmd
-}
